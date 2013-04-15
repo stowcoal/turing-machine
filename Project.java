724a3d0b4ba1be372227f70a205ea3e360db6ce8
stowcoal <cdstochl@gmail.com>
2013-04-14 20:39:15 -0500

    Update Project.java
    added jframe stuff

--- Project.java
+++ Project.java
@@ -1,8 +1,20 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class Project
 {
     public static void main(String args[])
     {
 	TuringMachine machine = new TuringMachine("111011");
+	JFrame f = new JFrame("This is a test");
+        f.setSize(400, 150);
+        Container content = f.getContentPane();
+        content.setBackground(Color.white);
+        content.setLayout(new FlowLayout());
+        content.add(new JButton("Button 1"));
+        content.add(new JButton("Button 2"));
+        content.add(new JButton("Button 3"));
+        f.setVisible(true);
 	machine.printConfigs();
 	machine.printTape();
 	machine.run("qf");
